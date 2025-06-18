75e4b3b73e87aeeb7b96873f0812a69c41673503
ksangoun <kasira40269@gmail.com>
2025-06-18 08:33:02 +0000

    HELLO35

---
+++ cell06/ex01/upcase_it.py
@@ -0,0 +1,14 @@
+def upcase_it(input_string):
+    """
+    Converts the input string to uppercase.
+
+    Args:
+        input_string: The string to convert.
+
+    Returns:
+        The uppercase version of the input string.
+    """
+    return input_string.upper()
+
+# Test the upcase_it method
+print(upcase_it("hello"))
